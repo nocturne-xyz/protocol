3ac54c09402d4bbb3527216ded6270bfbc3524b8
Luke Tchang <ltchang@stanford.edu>
2023-10-26 22:29:59 -0400

    fuzz tests ready

--- packages/contracts/contracts/test/fuzz/UniswapV3Adapter.t.sol
+++ packages/contracts/contracts/test/fuzz/UniswapV3Adapter.t.sol
@@ -14,8 +14,8 @@ contract UniswapV3AdapterTest is Test {
         uniswapAdapter = new TestUniswapV3Adapter(dummySwapRouter);
     }
 
-    function generateRandomPath(uint256 seed) internal returns(bytes memory path, address[] memory tokens, uint24[] memory fees) {
-        uint256 numHops = bound(seed, 1, 9);
+    function generateRandomPath(uint256 seed, uint256 minNumHops) internal returns(bytes memory path, address[] memory tokens, uint24[] memory fees) {
+        uint256 numHops = bound(seed, minNumHops, 9);
 
         tokens = new address[](numHops + 1);
         fees = new uint24[](numHops);
@@ -38,64 +38,38 @@ contract UniswapV3AdapterTest is Test {
         }
     }
 
-    function testSettingSwapRouter() public {
-        assertEq(
-            address(uniswapAdapter._swapRouter()),
-            dummySwapRouter,
-            "SwapRouter should be set to dummySwapRouter"
-        );
-    }
-
-    function testExtractTokenAddressFromPath() public {
-        address dummyToken1 = address(0xaBCdEf123456789012345678901234567890AbCd);
-        address dummyToken2 = address(0xfEDCbA987654321098765432109876543210fEdC);
-        
-        bytes memory path = abi.encodePacked(dummyToken1, uint24(0x000003), dummyToken2);
-        
-        address extractedToken1 = uniswapAdapter.testExtractTokenAddressFromPath(path, 0);
-        assertEq(extractedToken1, dummyToken1, "First token extracted should be dummyToken1");
-        
-        address extractedToken2 = uniswapAdapter.testExtractTokenAddressFromPath(path, 23);  // 23 bytes offset to get to the next token
-        assertEq(extractedToken2, dummyToken2, "Second token extracted should be dummyToken2");
-    }
-
-    function testTokenInWhitelist() public {
-        address dummyToken = address(
-            0xaBCdEf123456789012345678901234567890AbCd
-        );
-        uniswapAdapter.addToWhitelist(dummyToken);
-
-        bool isInWhitelist = uniswapAdapter.tokensAreAllowed(
-            abi.encodePacked(dummyToken, uint24(0x000003), dummyToken)
-        );
-        assertTrue(isInWhitelist, "Token should be in whitelist");
-    }
-
-    function testFuzz_ArbitraryTokenPath(uint256 seed) public {
-        (bytes memory path, address[] memory tokens, uint24[] memory fees) = generateRandomPath(seed);
+    function testFuzz_ArbitraryTokenPathExtraction(uint256 seed) public {
+        (bytes memory path, address[] memory tokens, uint24[] memory fees) = generateRandomPath(seed, 1);
         uint256 numHops = fees.length;
 
         for (uint256 i = 0; i < numHops; i++) {
             address extractedToken = uniswapAdapter.testExtractTokenAddressFromPath(path, i * 23);
-            assertEq(extractedToken, tokens[i], "Token extracted should be tokens[i]");
+            assertEq(extractedToken, tokens[i]);
         }
     }
 
-    function testFuzz_TokensNotInWhitelist() public {
-        (bytes memory path, address[] memory tokens, uint24[] memory fees) = generateRandomPath(seed);
-        uint256 numHops = fees.length;
+    function testFuzz_TokensInWhitelist(uint256 seed) public {
+        (bytes memory path, address[] memory tokens,) = generateRandomPath(seed, 1);
 
-        uniswapAdapter.addToWhitelist(tokens[0]);
-
-        address dummyToken1 = address(
-            0xaBCdEf123456789012345678901234567890AbCd
-        );
-        address dummyToken2 = address(
-            0xfEDCbA987654321098765432109876543210fEdC
-        );
+        for (uint256 i = 0; i < tokens.length; i++) {
+            uniswapAdapter.addToWhitelist(tokens[i]);
+        }
 
         bool isInWhitelist = uniswapAdapter.tokensAreAllowed(
-            abi.encodePacked(dummyToken1, uint24(0x000003), dummyToken2)
+            path
+        );
+        assertTrue(isInWhitelist);
+    }
+
+    function testFuzz_TokensNotInWhitelist(uint256 seed) public {
+        (bytes memory path, address[] memory tokens, uint24[] memory fees) = generateRandomPath(seed, 2);
+
+        uint256 tokenToWhitelistIndex = bound(seed, 0, fees.length);
+
+        uniswapAdapter.addToWhitelist(tokens[tokenToWhitelistIndex]);
+
+        bool isInWhitelist = uniswapAdapter.tokensAreAllowed(
+            path
         );
         assertFalse(isInWhitelist, "Token should not be in whitelist");
     }
