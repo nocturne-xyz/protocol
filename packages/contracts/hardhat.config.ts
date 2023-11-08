2b8d870da66572ca8cc426e6d09952670de4b798
Luke Tchang <ltchang@stanford.edu>
2023-11-08 11:18:08 -0500

    yarn i, add mainnet to config

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -66,6 +66,9 @@ module.exports = {
       url: `${process.env.GOERLI_RPC_URL}`,
       accounts: [`${process.env.DEPLOYER_KEY ?? DUMMY_KEY}`],
     },
+    homestead: {
+      url: `${process.env.MAINNET_RPC_URL}`,
+    },
   },
 
   etherscan: {
